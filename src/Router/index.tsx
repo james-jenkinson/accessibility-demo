0f99bb1bd47fd808439e9739e856058c018655b2
James Jenkinson <j.a.jenkinson@gmx.com>
2021-12-18 23:51:38 +0100

    Log basename

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -7,13 +7,17 @@ import {
 import Layout from '../Layout'
 import Semantics from '../routes/Semantics'
 
-const AppRouter: React.FC = () =>
-  <Router basename={BASENAME}>
+
+const AppRouter: React.FC = () =>{
+  console.log('basename', BASENAME)
+
+  return <Router basename={BASENAME}>
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Semantics />} />
       </Route>
     </Routes>
   </Router>
+}
 
 export default AppRouter
