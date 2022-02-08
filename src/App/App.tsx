a444e9a172924f2fa5cff4595355fb416b0cfbba
James Jenkinson <j.a.jenkinson@gmx.com>
2022-02-08 13:30:40 +0100

    Fix internationalisation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import AppRouter from '../Router'
-import '../i18n'
+import initI18n from '../i18n'
 import LanguageContext from '../contexts/LanguageContext'
 import './App.css'
+
+initI18n()
 
 const App: React.FC = () => (
   <div className='application-root'>
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,25 +4,28 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: false,
-    backend: {
-      loadPath: 'locales/{{lng}}/{{ns}}.json'
-    },
-    react: {
-      useSuspense: false
-    },
-    interpolation: {
-      escapeValue: false
-    }
-  })
-  .catch(err => {
-    console.log(err)
-  })
+const initI18n = () => {
+  i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      fallbackLng: 'en',
+      debug: false,
+      backend: {
+        loadPath: 'locales/{{lng}}/{{ns}}.json'
+      },
+      react: {
+        useSuspense: false
+      },
+      interpolation: {
+        escapeValue: false
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
-export default i18n
+
+export default initI18n
